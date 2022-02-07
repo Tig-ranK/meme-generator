5e3062b8e6f8552b3614f4cf64581cb0019d32cb
Tig-ranK <karakhanyantigran@outlook.com>
2022-02-08 01:23:15 +0400

    replaced local data with API

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import memesData from '../memesData';
 
 export default function Meme() {
   const [meme, setMeme] = useState({
@@ -9,44 +8,49 @@ export default function Meme() {
   });
   const [allMemes, setAllMemes] = useState('');
 
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+  
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
-    const rand = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[rand].url;
+    const rand = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[rand].url;
     setMeme((prevMeme) => ({ ...prevMeme, imageUrl: url }));
   }
 
   function handleChange(event) {
-    const {name, value} = event.target
-    setMeme(prevMeme => ({
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
       ...prevMeme,
-      [name]: value
-    }))
+      [name]: value,
+    }));
   }
 
   return (
     <main>
       <div className='form'>
         <div className='form-inputs'>
-          <input 
-            type='text' 
-            placeholder='Top text' 
+          <input
+            type='text'
+            placeholder='Top text'
             value={meme.topText}
-            name="topText"
+            name='topText'
             onChange={handleChange}
           />
-          <input 
-            type='text' 
-            placeholder='Bottom text' 
+          <input
+            type='text'
+            placeholder='Bottom text'
             value={meme.bottomText}
-            name="bottomText"
+            name='bottomText'
             onChange={handleChange}
           />
         </div>
         <button className='form-button' onClick={getMemeImage}>
           Get a new meme image 🖼️
         </button>
-        <img src={meme.imageUrl} alt="meme" />
+        <img src={meme.imageUrl} alt='meme' />
       </div>
     </main>
   );
