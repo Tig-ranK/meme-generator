b22f65928b90470e973b321f5945ab9a6d295611
Tig-ranK <karakhanyantigran@outlook.com>
2022-02-11 02:28:15 +0400

    still trying out fabric.js

--- src/App.js
+++ src/App.js
@@ -12,14 +12,38 @@ export default function App() {
 
   const initCanvas = () =>
     new fabric.Canvas('canvas', {
+      width: 500,
+      height: 500,
       backgroundColor: 'purple',
     });
 
+  const addRect = (canvi) => {
+    const rect = new fabric.Rect({
+      height: 200,
+      width: 200,
+      fill: 'yellow',
+    });
+    canvi.add(rect);
+    canvi.renderAll();
+  };
+
+  const addImage = (canvi) => {
+    const image = new fabric.Image.fromURL(
+      'https://assets-global.website-files.com/6005fac27a49a9cd477afb63/6057684e5923ad2ae43c8150_bavassano_homepage_before.jpg',
+      (img) => {
+        img.scale(0.1);
+        canvas.add(img);
+        canvas.renderAll();
+      }
+    );
+  };
   return (
     <div className='container'>
       <Header />
       <main className='main'>
         {/* <Meme /> */}
+        <button onClick={() => addRect(canvas)}>Rectangle</button>
+        <button onClick={() => addImage(canvas)}>Image</button>
         <canvas id='canvas'></canvas>
       </main>
     </div>
