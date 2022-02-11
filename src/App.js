0d4859d7f89771418e14bd1a1aa56e245b850a39
Tig-ranK <karakhanyantigran@outlook.com>
2022-02-11 16:12:41 +0400

    almost done with the editor

--- src/App.js
+++ src/App.js
@@ -1,72 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import { fabric } from 'fabric';
+// components
 import Header from './components/Header';
 import Meme from './components/Meme';
-import { fabric } from 'fabric';
 
 export default function App() {
-  // states
+  const defaultImage = 'http://i.imgflip.com/1bij.jpg';
   const [canvas, setCanvas] = useState('');
-  const [rect, setRect] = useState(
-    new fabric.Rect({
-      height: 100,
-      width: 100,
-      fill: `#ff7f00`,
-    })
-  );
-  // initiating the canvas
+
+  // initialize canvas
   useEffect(() => {
     setCanvas(
       () =>
         new fabric.Canvas('canvas', {
-          height: 500,
           width: 500,
+          height: 500,
           backgroundColor: 'lightblue',
+          preserveObjectStacking: true,
         })
     );
   }, []);
-  // failed attempt at resize observer
-  useEffect(() => {
-    console.log('rect changed');
-  }, [rect.getScaledWidth]);
 
-  const addRect = (canvi) => {
-    canvi.add(rect);
-    canvi.renderAll();
+  const addBackgroundImage = (canvi) => {
+    const image = new fabric.Image.fromURL(defaultImage, (img) => {
+      img.scale(1);
+      img.set('selectable', false);
+      canvi.add(img);
+      canvi.sendToBack(img);
+      canvi.renderAll();
+    });
   };
-
-  const addImage = (canvi) => {
-    const image = new fabric.Image.fromURL(
-      'https://assets-global.website-files.com/6005fac27a49a9cd477afb63/6057684e5923ad2ae43c8150_bavassano_homepage_before.jpg',
-      (img) => {
-        img.scale(0.1);
-        canvi.add(img);
-        canvi.renderAll();
-      }
-    );
-  };
-
-  const addText = canvi => {
-    const text = new fabric.IText("shut up".toUpperCase(), {
+  
+  const addText = (canvi) => {
+    const text = new fabric.IText('shut up', {
       top: 100,
       left: 100,
       fill: '#fff',
       stroke: '#000',
       strokeWidth: 0.7,
-      fontStyle: 'bold',
+      fontStyle: 'normal',
       fontFamily: 'Impact',
       fontSize: 32,
-      lineHeight: 39,      
-    })
-    canvi.add(text)
-    canvi.renderAll()
-  }
+    });
+    canvi.add(text);
+    canvi.sendToFront(text);
+    canvi.renderAll();
+  };
+
   return (
     <div className='container'>
       <Header />
       <main className='main'>
         {/* <Meme /> */}
-        <button onClick={() => addRect(canvas)}>Rectangle</button>
-        <button onClick={() => addImage(canvas)}>Image</button>
+        <button onClick={() => addBackgroundImage(canvas)}>Background</button>
         <button onClick={() => addText(canvas)}>Text</button>
         <canvas id='canvas'></canvas>
       </main>
