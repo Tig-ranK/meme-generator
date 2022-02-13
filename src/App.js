fb155d290f69a75cb416089265213f340ba693f3
Tig-ranK <karakhanyantigran@outlook.com>
2022-02-13 20:47:25 +0400

    added working canvas editting and download, need to merge it with existing meme component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { fabric } from 'fabric';
+import { saveAs } from 'file-saver';
 // components
 import Header from './components/Header';
 import Meme from './components/Meme';
 
 export default function App() {
   const defaultImage = 'http://i.imgflip.com/1bij.jpg';
-  const [canvas, setCanvas] = useState('');
+
+  const [canvas, setCanvas] = useState(
+    new fabric.Canvas('canvas', {
+      width: 500,
+      height: 500,
+      backgroundColor: 'lightblue',
+      preserveObjectStacking: true,
+    })
+  );
 
   // initialize canvas
   useEffect(() => {
@@ -22,39 +31,57 @@ export default function App() {
   }, []);
 
   const addBackgroundImage = (canvi) => {
-    const image = new fabric.Image.fromURL(defaultImage, (img) => {
-      img.scale(1);
-      img.set('selectable', false);
-      canvi.add(img);
-      canvi.sendToBack(img);
-      canvi.renderAll();
-    });
+    fabric.Image.fromURL(
+      defaultImage,
+      function (img) {
+        const image = img.set({
+          selectable: false,
+          evented: false,
+        });
+        canvi.add(image);
+        canvi.sendToBack(image);
+        canvi.renderAll();
+      },
+      { crossOrigin: 'anonymous' }
+    );
   };
-  
+
   const addText = (canvi) => {
-    const text = new fabric.IText('shut up', {
+    const text = new fabric.IText('shut up'.toUpperCase(), {
       top: 100,
       left: 100,
       fill: '#fff',
       stroke: '#000',
-      strokeWidth: 0.7,
+      strokeWidth: 1.5,
       fontStyle: 'normal',
       fontFamily: 'Impact',
       fontSize: 32,
     });
     canvi.add(text);
-    canvi.sendToFront(text);
+    // canvi.sendToFront(text);
     canvi.renderAll();
+  };
+
+  const saveImage = () => {
+    const dataURL = canvas.toDataURL({
+      format: 'png',
+      multiplier: 1,
+    });
+    saveAs(dataURL, 'meme.png');
   };
 
   return (
     <div className='container'>
       <Header />
       <main className='main'>
-        {/* <Meme /> */}
+        <Meme />
         <button onClick={() => addBackgroundImage(canvas)}>Background</button>
         <button onClick={() => addText(canvas)}>Text</button>
         <canvas id='canvas'></canvas>
+
+        <button className='form-button' onClick={saveImage}>
+          Download
+        </button>
       </main>
     </div>
   );
