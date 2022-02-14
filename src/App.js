a8d112805025d7dbc796d29f731101a6af3344e5
Tig-ranK <karakhanyantigran@outlook.com>
2022-02-14 15:05:23 +0400

    canvas works, refactoring the image/txt input mechanism

--- src/App.js
+++ src/App.js
@@ -1,87 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { fabric } from 'fabric';
-import { saveAs } from 'file-saver';
 // components
 import Header from './components/Header';
 import Meme from './components/Meme';
 
 export default function App() {
-  const defaultImage = 'http://i.imgflip.com/1bij.jpg';
-
-  const [canvas, setCanvas] = useState(
-    new fabric.Canvas('canvas', {
-      width: 500,
-      height: 500,
-      backgroundColor: 'lightblue',
-      preserveObjectStacking: true,
-    })
-  );
-
-  // initialize canvas
-  useEffect(() => {
-    setCanvas(
-      () =>
-        new fabric.Canvas('canvas', {
-          width: 500,
-          height: 500,
-          backgroundColor: 'lightblue',
-          preserveObjectStacking: true,
-        })
-    );
-  }, []);
-
-  const addBackgroundImage = (canvi) => {
-    fabric.Image.fromURL(
-      defaultImage,
-      function (img) {
-        const image = img.set({
-          selectable: false,
-          evented: false,
-        });
-        canvi.add(image);
-        canvi.sendToBack(image);
-        canvi.renderAll();
-      },
-      { crossOrigin: 'anonymous' }
-    );
-  };
-
-  const addText = (canvi) => {
-    const text = new fabric.IText('shut up'.toUpperCase(), {
-      top: 100,
-      left: 100,
-      fill: '#fff',
-      stroke: '#000',
-      strokeWidth: 1.5,
-      fontStyle: 'normal',
-      fontFamily: 'Impact',
-      fontSize: 32,
-    });
-    canvi.add(text);
-    // canvi.sendToFront(text);
-    canvi.renderAll();
-  };
-
-  const saveImage = () => {
-    const dataURL = canvas.toDataURL({
-      format: 'png',
-      multiplier: 1,
-    });
-    saveAs(dataURL, 'meme.png');
-  };
-
   return (
     <div className='container'>
       <Header />
       <main className='main'>
         <Meme />
-        <button onClick={() => addBackgroundImage(canvas)}>Background</button>
-        <button onClick={() => addText(canvas)}>Text</button>
-        <canvas id='canvas'></canvas>
-
-        <button className='form-button' onClick={saveImage}>
-          Download
-        </button>
       </main>
     </div>
   );
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import { saveAs } from 'file-saver';
+import { fabric } from 'fabric';
 
 export default function Meme() {
+  const defaultImage = 'http://i.imgflip.com/1bij.jpg';
   const [meme, setMeme] = useState({
-    topText: '',
-    bottomText: '',
-    imageUrl: 'http://i.imgflip.com/1bij.jpg',
+    imageUrl: defaultImage,
   });
   const [allMemes, setAllMemes] = useState('');
+  const [currentImage, setCurrentImage] = useState(defaultImage);
+  const [mainWidth, mainHeight] = [500, 500];
+  const [canvas, setCanvas] = useState(
+    new fabric.Canvas('canvas', {
+      width: mainWidth,
+      height: mainHeight,
+      backgroundColor: 'lightblue',
+      preserveObjectStacking: true,
+    })
+  );
+  // initialize canvas
+  useEffect(() => {
+    console.log('set the canvas');
+    setCanvas(
+      () =>
+        new fabric.Canvas('canvas', {
+          width: mainWidth,
+          height: mainHeight,
+          backgroundColor: 'lightblue',
+          preserveObjectStacking: true,
+        })
+    );
+  }, []);
+
+  // useEffect(() => {
+  //   window.addEventListener('resize', function fitResponsiveCanvas() {
+  //     // canvas dimensions
+  //     let canvasSize = {
+  //       width: 1200,
+  //       height: 700,
+  //     };
+  //     // canvas container dimensions
+  //     let containerSize = {
+  //       width: document.getElementById('canvas-container').offsetWidth,
+  //       height: document.getElementById('canvas-container').offsetHeight,
+  //     };
+  //     canvas.setDimensions(containerSize);
+  //     // how you want to handle your zoom is really application dependant.
+  //     let scaleRatio = Math.min(
+  //       containerSize.width / canvasSize.width,
+  //       containerSize.height / canvasSize.height
+  //     );
+  //     canvas.setZoom(scaleRatio);
+  //   });
+  // }, []);
 
   // fetching the array of memes
   useEffect(() => {
@@ -20,51 +65,76 @@ export default function Meme() {
   function getMeme() {
     const rand = Math.floor(Math.random() * allMemes.length);
     const url = allMemes[rand].url;
-    setMeme((prevMeme) => ({ ...prevMeme, imageUrl: url }));
+    setMeme({ imageUrl: url });
   }
 
-  // handle change of the inputs' text
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setMeme((prevMeme) => ({
-      ...prevMeme,
-      [name]: value,
-    }));
-  }
+  const addBackgroundImage = (canvi, url) => {
+    fabric.Image.fromURL(
+      url,
+      function (img) {
+        const image = img.set({
+          selectable: true,
+          scaleY: canvi.height / img.height,
+          scaleX: canvi.width / img.width,
+        });
+        canvi.add(image);
+        canvi.sendToBack(image);
+        canvi.renderAll();
+      },
+      { crossOrigin: 'anonymous' }
+    );
+  };
+
+  const addText = (canvi) => {
+    const text = new fabric.IText('shut up'.toUpperCase(), {
+      top: 100,
+      left: 100,
+      fill: '#fff',
+      stroke: '#000',
+      strokeWidth: 1.5,
+      fontStyle: 'normal',
+      fontFamily: 'Impact',
+      fontSize: 32,
+    });
+    canvi.add(text);
+    // canvi.sendToFront(text);
+    canvi.renderAll();
+  };
+
+  const saveImage = () => {
+    const dataURL = canvas.toDataURL({
+      format: 'png',
+      multiplier: 1,
+    });
+    saveAs(dataURL, 'meme.png');
+  };
+
+  const removeObject = () => {
+    canvas.remove(canvas.getActiveObject());
+  };
 
   return (
     <main>
       <div className='form'>
-        <div className='form-inputs'>
-          <input
-            type='text'
-            placeholder='Top text'
-            value={meme.topText}
-            name='topText'
-            onChange={handleChange}
-          />
-          <input
-            type='text'
-            placeholder='Bottom text'
-            value={meme.bottomText}
-            name='bottomText'
-            onChange={handleChange}
-          />
-        </div>
-
         <button className='form-button' onClick={getMeme}>
           Get a new meme image 🖼️
         </button>
-
-        <img className='meme-image' src={meme.imageUrl} alt='meme' />
       </div>
 
-      <button
-        className='form-button'
-        onClick={() => saveAs(meme.imageUrl, 'meme.png')}
-      >
-        Download
+      <button onClick={() => addBackgroundImage(canvas, meme.imageUrl)}>
+        Background
       </button>
+      <button onClick={() => addText(canvas)}>Text</button>
+
+      <canvas id='canvas' className='canvas'></canvas>
+      <div className='button-container'>
+        <button className='form-button' onClick={removeObject}>
+          Remove
+        </button>
+        <button className='form-button' onClick={saveImage}>
+          Download
+        </button>
+      </div>
     </main>
   );
 }
